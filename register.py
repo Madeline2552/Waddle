415b797d48382fc14b0dc9c4583bebf08a77d1c6
zyluke1994 <yzheng18@u.rochester.edu>
2014-10-09 17:44:52 -0400

    Create register.py

---
+++ register.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import cgi
+import os
+import Cookie
+import sqlite3
+import cgitb
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+un = form['un1'].value
+pw = form['pw1'].value
+em = form['em1'].value
+
+conn = sqlite3.connect('accounts.db')
+c = conn.cursor()
+
+c.execute('select * from users where name=?;', (un,))
+all_results = c.fetchall()
+if len(all_results)>0:
+    message = "Sorry, it seems like someone already registered using this username"
+else:
+    message = "wahoo!"
+    c.execute('insert into users values(?, ? ,?, ?);', (un, pw, em, ""))
+    conn.commit()
+
+
+#message = "YEEHAW!"
+print "Content-type: text/html"
+print
+
+print "<html>"
+print "<head><title>Registration</title></head>"
+print "<body>"
+print "<h1>" +message+"</h1>"
+print "<h2>your username is: " + un + " </h2>"
+print "<pre>"
+
+for row in c.execute('select * from users'):
+    print 'username: ', row[0], '| email: ', row[2]
+
+print "<pre>"
+print "</body>"
+print "</html>"
