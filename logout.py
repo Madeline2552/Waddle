5c4a793cef3259c2d977e5dc529e35142bedadea
Madeline2552 <25made.line@gmail.com>
2014-10-10 13:48:47 -0400

    Create logout.py
    Log out page that deletes session_id cookie.

---
+++ logout.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import cgi
+import sqlite3
+import cgitb
+import Cookie
+import os
+import uuid
+import time
+
+cgitb.enable()
+
+
+expires = time.time() - 14*24*3600
+#cookstring = os.environ.get('HTTP_COOKIE')
+
+my_cookie = Cookie.SimpleCookie()
+#session_id = str(uuid.uuid4())
+#saved_session_id = my_cookie['session_id'].value
+my_cookie['session_id'] = ' '
+my_cookie['session_id']['expires'] = time.strftime("%a, %d-%b-%Y %T GMT", time.gmtime(expires))
+
+
+
+print "Content-type: text/html"
+print my_cookie
+print 
+
+print"<html>"
+print"<head><title> Log Out</title></head>"
+print"<body>"
+print"<h1> You are now logged out. Thank you for visiting.</h1>"
+print "<a href='../index.html'> Back to Main Page </a>"
+print"</body>"
+print"</html>"
